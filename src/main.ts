d0f149f740476f2d6f7bfe2edd0114a8c37634f1
Hunghv <hunghvhpu@gmail.com>
2024-11-05 22:16:06 +0700

    asdasd

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as dotenvVault from 'dotenv-vault';
 
 async function bootstrap() {
+  dotenvVault.config();
+  dotenv.config();
   const app = await NestFactory.create(AppModule);
+  console.log(process.env);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
